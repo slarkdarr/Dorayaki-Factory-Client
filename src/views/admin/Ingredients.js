f530a476a368e023c7f50f1eff2187e2a33a7898
Faziz <farisaziz22@gmail.com>
2021-11-26 13:05:18 +0700

    add button

--- src/views/admin/Ingredients.js
+++ src/views/admin/Ingredients.js
@@ -8,7 +8,7 @@ export default function Ingredients() {
   return (
     <>
       <div className="flex flex-wrap mt-4">
-        <button className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded bg-white"> Halo gais</button>
+        <button className="relative flex flex-row-reverse flex min-w-0 bg-orange-500 text-white active:bg-orange-600 font-bold uppercase text-base px-8 py-3 rounded shadow-md hover:shadow-lg outline-none focus:outline-none mb-1 ease-linear transition-all duration-150 p-3 ml-3 mb-2"> Add</button>
         <div className="w-full mb-12 px-4">
           <CardTableIngredients />
         </div>
--- src/views/admin/Recipes.js
+++ src/views/admin/Recipes.js
@@ -8,6 +8,7 @@ export default function Recipes() {
   return (
     <>
       <div className="flex flex-wrap mt-4">
+        <button className="relative flex flex-row-reverse flex min-w-0 bg-orange-500 text-white active:bg-orange-600 font-bold uppercase text-base px-8 py-3 rounded shadow-md hover:shadow-lg outline-none focus:outline-none mb-1 ease-linear transition-all duration-150 p-3 ml-3 mb-2"> Add</button>
         <div className="w-full mb-12 px-4">
           <CardTableRecipes />
         </div>
