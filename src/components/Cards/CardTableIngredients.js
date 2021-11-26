3c8ff21418b207e7b5f176254be626da8f0cdd33
mhmmdjafarg <mjafarashadiq712@gmail.com>
2021-11-26 08:51:13 +0700

    add button styling

--- src/components/Cards/CardTableIngredients.js
+++ src/components/Cards/CardTableIngredients.js
@@ -63,7 +63,7 @@ const CardTableIngredients = () => {
       {
         name: "Action",
         // eslint-disable-next-line react/button-has-type
-        cell: row => <button onClick={openModal}>Edit Stock</button>
+        cell: row => <button className="bg-lightBlue-500 text-white active:bg-lightBlue-600 font-bold uppercase text-xs px-4 py-2 rounded-full shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" onClick={openModal}>Edit Stock</button>
       }
     ],
     []
