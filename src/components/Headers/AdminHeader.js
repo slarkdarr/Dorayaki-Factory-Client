a7dbb5c9d62f6145d409ab1e1c6203c14a774a09
slarkdarr <daffaresyaly@gmail.com>
2021-11-26 14:36:16 +0700

    design dashboard

--- src/components/Headers/AdminHeader.js
+++ src/components/Headers/AdminHeader.js
@@ -8,9 +8,11 @@ export default function AdminHeader() {
       {/* Header */}
       <div className="relative bg-lightBlue-600 md:pt-32 pb-32 pt-12 z-0">
         <div className="px-4 md:px-10 mx-auto w-full">
-          <h3 className="">
-            Welcome, Admin!
-          </h3>
+          <div className="flex flex-wrap">
+            <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4 text-2xl text-blueGray-200">
+              Hello, Admin!
+            </div>
+          </div>
         </div>
       </div>
     </>
