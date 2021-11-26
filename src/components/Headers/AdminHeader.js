fb10297f72a3ba58ee943501a755a1e984f58baf
mhmmdjafarg <mjafarashadiq712@gmail.com>
2021-11-26 12:53:33 +0700

    as

--- src/components/Headers/AdminHeader.js
+++ src/components/Headers/AdminHeader.js
@@ -6,7 +6,7 @@ export default function AdminHeader() {
   return (
     <>
       {/* Header */}
-      <div className="relative bg-lightBlue-600 md:pt-32 pb-32 pt-12">
+      <div className="relative bg-lightBlue-600 md:pt-32 pb-32 pt-12 z-0">
         <div className="px-4 md:px-10 mx-auto w-full">
           <h3 className="">
             Welcome, Admin!
--- src/views/admin/Ingredients.js
+++ src/views/admin/Ingredients.js
@@ -4,13 +4,13 @@ import React from "react";
 
 import CardTableIngredients from "components/Cards/CardTableIngredients.js";
 
-
 export default function Ingredients() {
   return (
     <>
       <div className="flex flex-wrap mt-4">
+        <button className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded bg-white"> Halo gais</button>
         <div className="w-full mb-12 px-4">
-          <CardTableIngredients/>
+          <CardTableIngredients />
         </div>
       </div>
     </>
